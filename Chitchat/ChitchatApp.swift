6f39a5f78690b72de227302020cae88c29d88bc9
yuhangang <redrainhang@gmail.com>
2026-05-07 15:13:07 +0800

    Initial Commit

---
+++ Chitchat/ChitchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChitchatApp.swift
+//  Chitchat
+//
+//  Created by Ang Yu Hang on 07/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChitchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chitchat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chitchat
+//
+//  Created by Ang Yu Hang on 07/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
